64a99ff41e1b4295899fa985deab7ad1ab366dd9
Murilo Júlio da Silveira Negrão <murilonegrao@gmail.com>
2024-01-02 22:11:17 -0300

    Commit Fundamentos

---
+++ 2_FUNDAMENTOS/aprendendoFundamentos/src/components/newTemplate.jsx
@@ -0,0 +1,11 @@
+const NewTemplate = () => {
+    const name = 'Leo';
+
+    return (
+        <div>
+            <h2>Olá, {name}, como está?</h2>
+        </div>
+    );
+};
+
+export default NewTemplate
